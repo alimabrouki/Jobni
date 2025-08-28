01ba2e0c0752ff6ee08f47da59a62204ed16c70e
alimab <alimabrouki9797@gmail.com>
2025-08-28 12:47:16 +0100

    i added logo and an icon bars and made it interactive to show menu list in mobile

---
+++ js/home.js
@@ -0,0 +1,19 @@
+
+const bars = document.getElementById('bars');
+const naviMenu = document.querySelector('.nav');
+const menu = document.getElementById('menu');
+const closeMenu = document.getElementById('close-menu');
+
+closeMenu.addEventListener('click', () => {
+  naviMenu.classList.remove('active')
+})
+bars.addEventListener('click', () => {
+  naviMenu.classList.toggle('active')
+});
+
+
+
+
+
+
+
