0b03f7da03901e521797202b7bc3147a931677b3
alimab <alimabrouki9797@gmail.com>
2025-08-30 17:38:02 +0100

    added the 2 buttons in js and make it interactive

---
+++ js/jobs.js
@@ -0,0 +1,32 @@
+const btn1 = document.querySelector('.js-btn1')
+const btn2 = document.querySelector('.js-btn2')
+
+function clickButton(active) {
+  if (active === 'btn1') {
+    btn1.classList.add('btn1');
+    btn1.classList.remove('btn1unclicked');
+    btn2.classList.add('btn2');
+    btn2.classList.remove('btn1unclicked')
+
+  } else  {
+    btn1.classList.add('btn1unclicked');
+    btn1.classList.remove('btn1');
+    btn2.classList.add('btn2clicked')
+    btn2.classList.remove('btn2')
+  }
+ localStorage.setItem('activebutton',active)
+}
+
+
+btn1.addEventListener('click' ,() => {
+   clickButton('btn1')
+})
+btn2.addEventListener('click', () => {
+  clickButton('btn2')
+})
+window.addEventListener('DOMContentLoaded', () => {
+  const saved = localStorage.getItem('activebutton');
+  if (saved) {
+    clickButton(saved)
+  }
+} )
